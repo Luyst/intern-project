6fcf4cabd078cb1a9479e319bef8b514b31ebceb
Luyst <dhuy15072003@gmail.com>
2024-09-30 12:38:28 +0700

    create project2

---
+++ queue-management/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="text-xl font-bold text-blue-500">Queue Management</div>
+        </div>
+    );
+}
+
+export default App;
---
+++ queue-management/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import tailwindcss from 'tailwindcss';
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '~': resolve(__dirname, 'src'), // Thay '@' bằng đường dẫn tới thư mục 'src'
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
+});
